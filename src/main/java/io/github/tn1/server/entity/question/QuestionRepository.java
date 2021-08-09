8bcbb4db739254bd8ec939a08e161e08ae6c98d1
leeseojune <sung07288346@gmail.com>
2021-08-10 00:29:38 +0900

    📑 :: Question model

---
+++ src/main/java/io/github/tn1/server/entity/question/QuestionRepository.java
@@ -0,0 +1,8 @@
+package io.github.tn1.server.entity.question;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+}
