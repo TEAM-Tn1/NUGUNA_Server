90bb931e9ee7d436961b9d2525d4a3e413973530
leeseojune <sung07288346@gmail.com>
2021-08-10 00:30:15 +0900

    📑 :: Report Medium model

---
+++ src/test/java/io/github/tn1/server/entity/report/medium/MediumTest.java
@@ -0,0 +1,35 @@
+package io.github.tn1.server.entity.report.medium;
+
+import io.github.tn1.server.entity.report.Report;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MediumTest {
+
+    @Test
+    @DisplayName("Builder를 활용하여 Medium객체를 생성하는 메소드")
+    void createByBuilder() {
+        //given
+        Report report = Report.builder()
+                .title("신고")
+                .build();
+
+        String path = "자료위치";
+
+        //when
+        Medium medium = Medium.builder()
+                .report(report)
+                .path(path)
+                .build();
+
+        //then
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(report, medium.getReport()),
+                () -> Assertions.assertEquals(path, medium.getPath())
+        );
+    }
+
+}
