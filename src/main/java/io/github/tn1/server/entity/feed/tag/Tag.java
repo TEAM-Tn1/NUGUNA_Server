4fd45382db536f92eb1fff69df6abbe46c2488a3
leeseojune <sung07288346@gmail.com>
2021-08-10 00:29:12 +0900

    📑 :: Tag model

---
+++ src/main/java/io/github/tn1/server/entity/feed/tag/Tag.java
@@ -0,0 +1,32 @@
+package io.github.tn1.server.entity.feed.tag;
+
+import io.github.tn1.server.entity.feed.Feed;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity(name = "tbl_tag")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 10)
+    private String tag;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id")
+    private Feed feed;
+
+    @Builder
+    public Tag(String tag, Feed feed) {
+        this.tag = tag;
+        this.feed = feed;
+    }
+
+}
