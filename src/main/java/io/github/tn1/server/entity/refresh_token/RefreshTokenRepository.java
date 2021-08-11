9723bb9b30eabe17bcd59e039a1a5b3b76cce8cb
leeseojune <sung07288346@gmail.com>
2021-08-12 02:54:25 +0900

    ⚡ :: Repository 어노테이션 명명

--- src/main/java/io/github/tn1/server/entity/refresh_token/RefreshTokenRepository.java
+++ src/main/java/io/github/tn1/server/entity/refresh_token/RefreshTokenRepository.java
@@ -1,6 +1,8 @@
 package io.github.tn1.server.entity.refresh_token;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 }
--- src/main/java/io/github/tn1/server/entity/user/UserRepository.java
+++ src/main/java/io/github/tn1/server/entity/user/UserRepository.java
@@ -1,6 +1,8 @@
 package io.github.tn1.server.entity.user;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, String> {
 }
