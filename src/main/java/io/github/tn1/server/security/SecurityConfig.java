03bf975e80d3cfa3c09afd74c145e16cb47d3545
leeseojune <sung07288346@gmail.com>
2021-08-11 23:46:01 +0900

    📑 :: Security Config

---
+++ src/main/java/io/github/tn1/server/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package io.github.tn1.server.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .formLogin().disable()
+                .csrf().disable()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+}
