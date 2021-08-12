eab2c65ab9bb205096013b5ba159a4b7c470b3a3
leeseojune <sung07288346@gmail.com>
2021-08-12 23:54:55 +0900

    📑 :: 토큰 재발급 Request Body

---
+++ src/main/java/io/github/tn1/server/dto/user/request/RefreshTokenRequest.java
@@ -0,0 +1,10 @@
+package io.github.tn1.server.dto.user.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class RefreshTokenRequest {
+    private String refreshToken;
+}
