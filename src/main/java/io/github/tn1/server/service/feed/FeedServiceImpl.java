de675738b8876d08b1e2187e6f2185eed233b3d4
leeseojune <sung07288346@gmail.com>
2021-08-13 03:57:22 +0900

    📑 :: FeedService 구현체

---
+++ src/main/java/io/github/tn1/server/service/feed/FeedServiceImpl.java
@@ -0,0 +1,14 @@
+package io.github.tn1.server.service.feed;
+
+import io.github.tn1.server.dto.feed.request.PostCarrotRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FeedServiceImpl implements FeedService {
+
+    @Override
+    public void postCarrotFeed(PostCarrotRequest request) {
+
+    }
+
+}
