0d77941b00885a0956bf07ad1721cef011f56b74
leeseojune <sung07288346@gmail.com>
2021-08-13 00:03:33 +0900

    ⚡ :: FeedController 당근 게시물 추가

--- src/main/java/io/github/tn1/server/controller/FeedController.java
+++ src/main/java/io/github/tn1/server/controller/FeedController.java
@@ -1,13 +1,22 @@
 package io.github.tn1.server.controller;
 
+import io.github.tn1.server.dto.feed.request.PostCarrotRequest;
 import io.github.tn1.server.service.feed.FeedService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/feed")
 @RequiredArgsConstructor
 public class FeedController {
 
     private final FeedService feedService;
+
+    @PostMapping("/carrot")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void postCarrotFeed(@RequestBody PostCarrotRequest request) {
+        feedService.postCarrotFeed(request);
+    }
 
 }
