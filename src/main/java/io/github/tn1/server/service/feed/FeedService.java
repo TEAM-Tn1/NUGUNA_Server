68489089fd2690702f7f707a0c17fa26ec3e2cec
leeseojune <sung07288346@gmail.com>
2021-08-13 00:03:24 +0900

    ⚡ :: FeedService interface 당근 게시물 등록 추가

--- src/main/java/io/github/tn1/server/service/feed/FeedService.java
+++ src/main/java/io/github/tn1/server/service/feed/FeedService.java
@@ -1,4 +1,7 @@
 package io.github.tn1.server.service.feed;
 
+import io.github.tn1.server.dto.feed.request.PostCarrotRequest;
+
 public interface FeedService {
+    void postCarrotFeed(PostCarrotRequest request);
 }
