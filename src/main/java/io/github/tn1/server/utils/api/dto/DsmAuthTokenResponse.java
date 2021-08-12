b9898c7f8dc9188ed0e3f36f4f16a8360fba0035
leeseojune <sung07288346@gmail.com>
2021-08-13 03:26:45 +0900

    📑 :: DsmAuth에 토큰을 받아오는 Api의 Response Body

---
+++ src/main/java/io/github/tn1/server/utils/api/dto/DsmAuthTokenResponse.java
@@ -0,0 +1,11 @@
+package io.github.tn1.server.utils.api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+public class DsmAuthTokenResponse {
+
+    private String accessToken;
+
+}
