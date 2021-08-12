25ce3cc0f8ecd3c072c16254a99804b4a73b33f3
leeseojune <sung07288346@gmail.com>
2021-08-13 01:12:57 +0900

    ⚡ :: UserController 오타 수정

--- src/main/java/io/github/tn1/server/controller/UserController.java
+++ src/main/java/io/github/tn1/server/controller/UserController.java
@@ -9,7 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
