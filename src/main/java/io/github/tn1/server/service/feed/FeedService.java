6d45a8cf1c7a6118b04c9cf69c0f07d54fe8f8d2
leeseojune <sung07288346@gmail.com>
2021-08-14 00:14:16 +0900

    ⚡ :: 유저가 작성한 게시물 가져오기 interface

--- src/main/java/io/github/tn1/server/service/feed/FeedService.java
+++ src/main/java/io/github/tn1/server/service/feed/FeedService.java
@@ -1,7 +1,11 @@
 package io.github.tn1.server.service.feed;
 
 import io.github.tn1.server.dto.feed.request.PostCarrotRequest;
+import io.github.tn1.server.dto.feed.response.WriteFeedResponse;
+
+import java.util.List;
 
 public interface FeedService {
     void postCarrotFeed(PostCarrotRequest request);
+    List<WriteFeedResponse> getWriteFeed(String email);
 }
