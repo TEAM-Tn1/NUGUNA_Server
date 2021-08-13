0372b8c72bf04a829cd76cbf67f493d89c346f33
leeseojune <sung07288346@gmail.com>
2021-08-14 00:14:46 +0900

    ⚡ :: Medium findTopByFeedOrderById

--- src/main/java/io/github/tn1/server/entity/feed/medium/FeedMediumRepository.java
+++ src/main/java/io/github/tn1/server/entity/feed/medium/FeedMediumRepository.java
@@ -1,8 +1,13 @@
 package io.github.tn1.server.entity.feed.medium;
 
+import io.github.tn1.server.entity.feed.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FeedMediumRepository extends JpaRepository<FeedMedium, Long> {
+    FeedMedium findTopByFeedOrderById(Feed feed);
+
 }
