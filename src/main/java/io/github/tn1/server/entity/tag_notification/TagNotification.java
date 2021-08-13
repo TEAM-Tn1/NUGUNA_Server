3c5ff56fd9d5b012b3371fe42fd7abd45f8c6d02
leeseojune <sung07288346@gmail.com>
2021-08-13 22:36:34 +0900

    📑 :: 태그 알람 엔티티 작성

---
+++ src/main/java/io/github/tn1/server/entity/tag_notification/TagNotification.java
@@ -0,0 +1,32 @@
+package io.github.tn1.server.entity.tag_notification;
+
+import io.github.tn1.server.entity.user.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity(name = "tbl_tag_notification")
+public class TagNotification {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(length = 10)
+    private String tag;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "email")
+    private User user;
+
+    @Builder
+    public TagNotification(String tag, User user) {
+        this.tag = tag;
+        this.user = user;
+    }
+
+}
