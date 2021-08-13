17408e165d8f5cd456c13860ea23e048efb611ce
leeseojune <sung07288346@gmail.com>
2021-08-13 22:15:43 +0900

    ⚡ :: ExpiredRefreshTokenException Enum 이름 변경

--- src/main/java/io/github/tn1/server/exception/ExpiredRefreshTokenException.java
+++ src/main/java/io/github/tn1/server/exception/ExpiredRefreshTokenException.java
@@ -6,7 +6,7 @@ import io.github.tn1.server.error.exception.ServerException;
 public class ExpiredRefreshTokenException extends ServerException {
 
     public ExpiredRefreshTokenException() {
-        super(ErrorCode.EXPIRED_REFRESHTOKEN);
+        super(ErrorCode.EXPIRED_REFRESH_TOKEN);
     }
 
 }
