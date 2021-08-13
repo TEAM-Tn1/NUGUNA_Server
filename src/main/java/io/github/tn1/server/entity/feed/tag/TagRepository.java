e744cad13898e46d288e375f9647bf043bbfcc7e
leeseojune <sung07288346@gmail.com>
2021-08-14 00:14:30 +0900

    ⚡ :: 정렬된 tag list 가져오기

--- src/main/java/io/github/tn1/server/entity/feed/tag/TagRepository.java
+++ src/main/java/io/github/tn1/server/entity/feed/tag/TagRepository.java
@@ -1,8 +1,12 @@
 package io.github.tn1.server.entity.feed.tag;
 
+import io.github.tn1.server.entity.feed.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    List<Tag> findByFeedOrderById(Feed feed);
 }
