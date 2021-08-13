8c18282d6ceecdb6ff5330b8390d4c95a8b36f77
leeseojune <sung07288346@gmail.com>
2021-08-14 00:14:08 +0900

    ⚡ :: Feed findByUser

--- src/main/java/io/github/tn1/server/entity/feed/FeedRepository.java
+++ src/main/java/io/github/tn1/server/entity/feed/FeedRepository.java
@@ -1,8 +1,12 @@
 package io.github.tn1.server.entity.feed;
 
+import io.github.tn1.server.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FeedRepository extends JpaRepository<Feed, Long> {
+    List<Feed> findByUser(User user);
 }
