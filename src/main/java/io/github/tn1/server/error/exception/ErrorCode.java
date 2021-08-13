2c5cdd5b2148914f0bae2d969c347d6771793f68
leeseojune <sung07288346@gmail.com>
2021-08-13 22:15:07 +0900

    ⚡ :: Blacked Enum 추가

--- src/main/java/io/github/tn1/server/error/exception/ErrorCode.java
+++ src/main/java/io/github/tn1/server/error/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
 
     USER_NOT_FOUND(404, "User not found."),
     INVALID_TOKEN(401, "Invalid token."),
-    EXPIRED_REFRESHTOKEN(401, "Expired refresh token.");
+    EXPIRED_REFRESH_TOKEN(401, "Expired refresh token."),
+    BLACKED(403, "User was blacked.");
 
     private final int status;
     private final String message;
