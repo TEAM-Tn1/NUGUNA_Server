6fde04dd37dc57201d5967f74eef5e4a0b572d4c
leeseojune <sung07288346@gmail.com>
2021-08-17 23:05:37 +0900

    ⚡ :: TagNotification findByTag

--- src/main/java/io/github/tn1/server/entity/tag_notification/TagNotificationRepository.java
+++ src/main/java/io/github/tn1/server/entity/tag_notification/TagNotificationRepository.java
@@ -3,6 +3,9 @@ package io.github.tn1.server.entity.tag_notification;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TagNotificationRepository extends CrudRepository<TagNotification, Long> {
+    List<TagNotification> findByTag(String tag);
 }
