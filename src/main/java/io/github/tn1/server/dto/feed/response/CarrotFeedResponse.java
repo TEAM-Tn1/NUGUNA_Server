ffd0540069998bf913a8c186d6a4271e048fc208
leeseojune <sung07288346@gmail.com>
2021-08-23 09:41:11 +0900

    📑 :: 당근마켓 게시물 Dto

---
+++ src/main/java/io/github/tn1/server/dto/feed/response/CarrotFeedResponse.java
@@ -0,0 +1,31 @@
+package io.github.tn1.server.dto.feed.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CarrotFeedResponse {
+
+    private Long feedId;
+    private String title;
+    private String description;
+    private Integer price;
+    private List<String> tags;
+    private String photo;
+    private LocalDateTime lastModifyDate;
+    private boolean like;
+    private Integer count;
+
+    public void setLike(boolean value) {
+        this.like = value;
+    }
+
+}
