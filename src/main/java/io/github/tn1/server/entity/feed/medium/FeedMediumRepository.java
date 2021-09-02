7eade0f63298961e265fdb28a64accaae57450d6
leeseojune <sung07288346@gmail.com>
2021-09-02 22:58:11 +0900

    ⚡ :: 해당하는 Feed의 Medium갯수

--- src/main/java/io/github/tn1/server/entity/feed/medium/FeedMediumRepository.java
+++ src/main/java/io/github/tn1/server/entity/feed/medium/FeedMediumRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface FeedMediumRepository extends JpaRepository<FeedMedium, Long> {
     FeedMedium findTopByFeedOrderById(Feed feed);
-
+    int countByFeed(Feed feed);
 }
