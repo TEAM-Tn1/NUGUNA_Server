8ad8cb669d65fd92ddf87fb81683db0c3a94e575
leeseojune <sung07288346@gmail.com>
2021-09-10 23:39:08 +0900

    ⚡ :: GeneratedValue strategy추가

--- src/main/java/io/github/tn1/server/entity/tag_notification/TagNotification.java
+++ src/main/java/io/github/tn1/server/entity/tag_notification/TagNotification.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class TagNotification {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 10)
