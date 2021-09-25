c849146f150d8e8ac1b5c3d53a7a4a8135b2a801
leeseojune <sung07288346@gmail.com>
2021-09-25 21:59:55 +0900

    🐛 :: import

--- src/main/java/io/github/tn1/server/entity/feed/tag/Tag.java
+++ src/main/java/io/github/tn1/server/entity/feed/tag/Tag.java
@@ -1,11 +1,19 @@
 package io.github.tn1.server.entity.feed.tag;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import io.github.tn1.server.entity.feed.Feed;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
