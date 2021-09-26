a359d943c254544e1126695091f22cf46c61f517
leeseojune <sung07288346@gmail.com>
2021-09-26 17:18:05 +0900

    ⚡ :: /chat prefix

--- src/main/java/io/github/tn1/server/controller/ChatController.java
+++ src/main/java/io/github/tn1/server/controller/ChatController.java
@@ -6,9 +6,11 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/chat")
 @RequiredArgsConstructor
 public class ChatController {
 
