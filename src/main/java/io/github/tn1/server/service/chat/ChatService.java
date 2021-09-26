163c8207e2be0d6ac57cdd5a10ddfb1eafa830dd
leeseojune <sung07288346@gmail.com>
2021-09-26 23:45:19 +0900

    ⚡ :: 서비스 적용

--- src/main/java/io/github/tn1/server/service/chat/ChatService.java
+++ src/main/java/io/github/tn1/server/service/chat/ChatService.java
@@ -5,14 +5,18 @@ import io.github.tn1.server.entity.chat.member.Member;
 import io.github.tn1.server.entity.chat.member.MemberRepository;
 import io.github.tn1.server.entity.chat.room.Room;
 import io.github.tn1.server.entity.chat.room.RoomRepository;
+import io.github.tn1.server.entity.chat.room.RoomType;
 import io.github.tn1.server.entity.feed.Feed;
 import io.github.tn1.server.entity.feed.FeedRepository;
+import io.github.tn1.server.entity.feed.medium.FeedMedium;
+import io.github.tn1.server.entity.feed.medium.FeedMediumRepository;
 import io.github.tn1.server.entity.user.User;
 import io.github.tn1.server.entity.user.UserRepository;
 import io.github.tn1.server.exception.FeedNotFoundException;
 import io.github.tn1.server.exception.RoomNotFoundException;
 import io.github.tn1.server.exception.UserNotFoundException;
 import io.github.tn1.server.security.facade.UserFacade;
+import io.github.tn1.server.utils.s3.S3Service;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
@@ -23,22 +27,29 @@ public class ChatService {
 
 	private final UserRepository userRepository;
 	private final FeedRepository feedRepository;
+	private final FeedMediumRepository feedMediumRepository;
 	private final RoomRepository roomRepository;
 	private final MemberRepository memberRepository;
+
+	private final S3Service s3Service;
 
 	public JoinResponse joinRoom(Long feedId) {
 		User currentUser = userRepository.findById(UserFacade.getEmail())
 				.orElseThrow(UserNotFoundException::new);
 		Feed feed = feedRepository.findById(feedId)
 				.orElseThrow(FeedNotFoundException::new);
+		FeedMedium medium = feedMediumRepository
+				.findTopByFeedOrderById(feed);
 		Room room;
 		if(feed.isUsedItem()) {
 			room = roomRepository.save(
 					Room.builder()
 					.feed(feed)
 					.user(feed.getUser())
+					.type(RoomType.CARROT)
+					.photoUrl(medium != null ?
+							s3Service.getObjectUrl(medium.getFileName()) : null)
 					.build());
-
 		} else {
 			room = roomRepository.findByFeed(feed)
 					.orElseThrow(RoomNotFoundException::new);
