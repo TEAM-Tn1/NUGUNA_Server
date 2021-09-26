64544584595ed6963b5289af69f5262c0988e445
leeseojune <sung07288346@gmail.com>
2021-09-26 19:42:02 +0900

    📑 :: JoinResponse Dto

---
+++ src/main/java/io/github/tn1/server/dto/chat/response/JoinResponse.java
@@ -0,0 +1,12 @@
+package io.github.tn1.server.dto.chat.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class JoinResponse {
+
+	private final String roomId;
+
+}
