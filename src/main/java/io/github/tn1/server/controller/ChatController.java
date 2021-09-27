b3da400084b642afc0f8a182381ab93b233efccc
leeseojune <sung07288346@gmail.com>
2021-09-27 09:34:03 +0900

    ⚡ :: /chat/carrot

--- src/main/java/io/github/tn1/server/controller/ChatController.java
+++ src/main/java/io/github/tn1/server/controller/ChatController.java
@@ -1,10 +1,14 @@
 package io.github.tn1.server.controller;
 
+import java.util.List;
+
 import io.github.tn1.server.dto.chat.request.JoinRequest;
+import io.github.tn1.server.dto.chat.response.CarrotRoomResponse;
 import io.github.tn1.server.dto.chat.response.JoinResponse;
 import io.github.tn1.server.service.chat.ChatService;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +24,11 @@ public class ChatController {
 	@PostMapping("/room")
 	public JoinResponse joinRoom(@RequestBody JoinRequest request) {
 		return chatService.joinRoom(request.getFeedId());
+	}
+
+	@GetMapping("/carrot")
+	public List<CarrotRoomResponse> getCarrotRoom() {
+		return chatService.getCarrotRoom();
 	}
 
 }
