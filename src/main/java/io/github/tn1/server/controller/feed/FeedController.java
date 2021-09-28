5f3abf591ff4a6db8a44076e4487c771b09958a7
leeseojune <sung07288346@gmail.com>
2021-09-28 23:41:19 +0900

    ♻ :: feed controller

---
+++ src/main/java/io/github/tn1/server/controller/feed/FeedController.java
@@ -0,0 +1,51 @@
+package io.github.tn1.server.controller.feed;
+
+import java.util.List;
+
+import io.github.tn1.server.dto.feed.response.FeedResponse;
+import io.github.tn1.server.service.feed.FeedService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/feed")
+@RequiredArgsConstructor
+public class FeedController {
+
+	private final FeedService feedService;
+
+	@DeleteMapping("/{feed_id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void removeFeed(@PathVariable(name = "feed_id") Long feedId) {
+		feedService.removeFeed(feedId);
+	}
+
+	@GetMapping("/{email}")
+	public List<FeedResponse> getWriteFeed(@PathVariable("email") String email) {
+		return feedService.getWriteFeed(email);
+	}
+
+	@PostMapping("/{feed_id}/photo")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void uploadPhoto(@RequestPart List<MultipartFile> files, @PathVariable("feed_id") Long feedId) {
+		feedService.uploadPhoto(files, feedId);
+	}
+
+	@DeleteMapping("/photo")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletePhoto(@RequestParam("file_name") String fileName) {
+		feedService.removePhoto(fileName);
+	}
+
+}
