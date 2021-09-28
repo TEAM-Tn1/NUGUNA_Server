67b6051fc8433634e8f6dfeee67ab22d08079b28
leeseojune <sung07288346@gmail.com>
2021-09-28 23:43:12 +0900

    ♻ :: Feed 분리

--- src/main/java/io/github/tn1/server/service/feed/FeedService.java
+++ src/main/java/io/github/tn1/server/service/feed/FeedService.java
@@ -1,20 +1,14 @@
 package io.github.tn1.server.service.feed;
 
-import io.github.tn1.server.dto.feed.request.ModifyCarrotRequest;
-import io.github.tn1.server.dto.feed.request.PostCarrotRequest;
-import io.github.tn1.server.dto.feed.response.FeedResponse;
-import io.github.tn1.server.dto.feed.response.WriteFeedResponse;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.util.List;
 
+import io.github.tn1.server.dto.feed.response.FeedResponse;
+
+import org.springframework.web.multipart.MultipartFile;
+
 public interface FeedService {
-    void postCarrotFeed(PostCarrotRequest request);
-    void modifyCarrotFeed(ModifyCarrotRequest request);
-    List<FeedResponse> getCarrotFeed(int page, int range);
     void removeFeed(Long id);
-    List<WriteFeedResponse> getWriteFeed(String email);
+    List<FeedResponse> getWriteFeed(String email);
     void uploadPhoto(List<MultipartFile> files, Long feedId);
-    List<FeedResponse> getLikedCarrot();
     void removePhoto(String path);
 }
