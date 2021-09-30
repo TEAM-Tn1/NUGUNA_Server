f3519e5484f5cc6a1c084945d1b035f9368e3d66
leeseojune <sung07288346@gmail.com>
2021-09-30 09:26:01 +0900

    📑 :: DeviceTokenRequest dto

---
+++ src/main/java/io/github/tn1/server/dto/user/request/DeviceTokenRequest.java
@@ -0,0 +1,12 @@
+package io.github.tn1.server.dto.user.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class DeviceTokenRequest {
+
+	private String deviceToken;
+
+}
