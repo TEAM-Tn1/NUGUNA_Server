89dcb867caa1462a2caf165ecb7170827ff39734
leeseojune <sung07288346@gmail.com>
2021-10-01 14:33:06 +0900

    ⚡ :: /feed/group

--- src/main/java/io/github/tn1/server/controller/feed/GroupFeedController.java
+++ src/main/java/io/github/tn1/server/controller/feed/GroupFeedController.java
@@ -1,13 +1,29 @@
 package io.github.tn1.server.controller.feed;
 
+import javax.validation.Valid;
+
+import io.github.tn1.server.dto.feed.request.PostGroupRequest;
+import io.github.tn1.server.service.feed.group.GroupFeedService;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/feed")
 @RequiredArgsConstructor
 public class GroupFeedController {
+
+	private final GroupFeedService groupFeedService;
+
+	@PostMapping("/group")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void postGroupFeed(@RequestBody @Valid PostGroupRequest request) {
+		groupFeedService.postGroupFeed(request);
+	}
 
 }
