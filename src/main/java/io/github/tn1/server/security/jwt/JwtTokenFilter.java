ef2916818ddeca94648ddbb141399fc1fa097419
leeseojune <sung07288346@gmail.com>
2021-10-01 14:27:38 +0900

    ⚡ :: filter에 Component 제거

--- src/main/java/io/github/tn1/server/security/jwt/JwtTokenFilter.java
+++ src/main/java/io/github/tn1/server/security/jwt/JwtTokenFilter.java
@@ -1,18 +1,18 @@
 package io.github.tn1.server.security.jwt;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-@Component
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 @RequiredArgsConstructor
 public class JwtTokenFilter extends OncePerRequestFilter {
 
