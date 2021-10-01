241ef49a5c38001c2e8f75e7ef3e2876cb003589
leeseojune <sung07288346@gmail.com>
2021-10-01 14:26:29 +0900

    ♻ :: UserFacade

--- src/main/java/io/github/tn1/server/facade/user/UserFacade.java
+++ src/main/java/io/github/tn1/server/facade/user/UserFacade.java
@@ -1,15 +1,15 @@
-package io.github.tn1.server.security.facade;
+package io.github.tn1.server.facade.user;
 
 import io.github.tn1.server.exception.CredentialsNotFoundException;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Component
 public class UserFacade {
 
-    public static String getEmail() {
+    public String getEmail() {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if(authentication == null || authentication.getPrincipal() == null)
             throw new CredentialsNotFoundException();
