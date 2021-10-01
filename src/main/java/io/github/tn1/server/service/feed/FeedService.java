002d97e000e81a2598e4e3532c4bc273eb6b97f1
leeseojune <sung07288346@gmail.com>
2021-10-01 14:28:49 +0900

    ♻ :: interface 제거

--- src/main/java/io/github/tn1/server/service/feed/FeedService.java
+++ src/main/java/io/github/tn1/server/service/feed/FeedService.java
@@ -1,16 +1,148 @@
 package io.github.tn1.server.service.feed;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import io.github.tn1.server.dto.feed.request.ModifyTagRequest;
 import io.github.tn1.server.dto.feed.response.FeedResponse;
+import io.github.tn1.server.entity.feed.Feed;
+import io.github.tn1.server.entity.feed.FeedRepository;
+import io.github.tn1.server.entity.feed.medium.FeedMedium;
+import io.github.tn1.server.entity.feed.medium.FeedMediumRepository;
+import io.github.tn1.server.entity.feed.tag.Tag;
+import io.github.tn1.server.entity.feed.tag.TagRepository;
+import io.github.tn1.server.entity.user.User;
+import io.github.tn1.server.entity.user.UserRepository;
+import io.github.tn1.server.exception.CredentialsNotFoundException;
+import io.github.tn1.server.exception.FeedNotFoundException;
+import io.github.tn1.server.exception.FileEmptyException;
+import io.github.tn1.server.exception.MediumNotFoundException;
+import io.github.tn1.server.exception.NotYourFeedException;
+import io.github.tn1.server.exception.TooManyFilesException;
+import io.github.tn1.server.exception.TooManyTagsException;
+import io.github.tn1.server.exception.UserNotFoundException;
+import io.github.tn1.server.facade.feed.FeedFacade;
+import io.github.tn1.server.facade.user.UserFacade;
+import io.github.tn1.server.utils.s3.S3Util;
+import lombok.RequiredArgsConstructor;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-public interface FeedService {
-    List<FeedResponse> queryWriteFeed(String email);
-    FeedResponse queryFeed(Long feedId);
-    void uploadPhoto(List<MultipartFile> files, Long feedId);
-	void removeFeed(Long id);
-    void modifyTag(ModifyTagRequest request);
+@Service
+@RequiredArgsConstructor
+public class FeedService {
+
+	private final UserFacade userFacade;
+	private final FeedFacade feedFacade;
+    private final S3Util s3Util;
+    private final FeedRepository feedRepository;
+    private final TagRepository tagRepository;
+    private final FeedMediumRepository feedMediumRepository;
+    private final UserRepository userRepository;
+
+    public List<FeedResponse> queryWriteFeed(String email) {
+        User writer = userRepository.findById(email)
+                .orElseThrow(UserNotFoundException::new);
+
+        User user = userRepository.findById(userFacade.getEmail())
+                .orElse(null);
+
+        return feedRepository.findByUser(writer)
+                .stream().map(feed -> feedFacade.feedToFeedResponse(feed, user))
+				.collect(Collectors.toList());
+    }
+
+	public FeedResponse queryFeed(Long feedId) {
+    	User user = userRepository.findById(userFacade.getEmail())
+				.orElse(null);
+    	Feed feed = feedRepository.findById(feedId)
+				.orElseThrow(FeedNotFoundException::new);
+    	return feedFacade.feedToFeedResponse(feed, user);
+	}
+
+	public void uploadPhoto(List<MultipartFile> files, Long feedId) {
+		User user = userRepository.findById(userFacade.getEmail())
+				.orElseThrow(CredentialsNotFoundException::new);
+
+		Feed feed = feedRepository.findById(feedId)
+				.orElseThrow(FeedNotFoundException::new);
+
+		if(!feed.getUser().getEmail().equals(user.getEmail()))
+			throw new NotYourFeedException();
+
+		if(files == null)
+			throw new FileEmptyException();
+
+		if(files.size() + feedMediumRepository.countByFeed(feed) > 5)
+			throw new TooManyFilesException();
+
+		files.forEach(file ->
+				feedMediumRepository.save(FeedMedium.builder()
+						.feed(feed)
+						.fileName(s3Util.upload(file))
+						.build())
+		);
+	}
+
+	public void removeFeed(Long id) {
+		User user = userRepository.findById(userFacade.getEmail())
+				.orElseThrow(CredentialsNotFoundException::new);
+
+		Feed feed = feedRepository.findById(id)
+				.orElseThrow(FeedNotFoundException::new);
+
+		if(!feed.getUser().getEmail().equals(user.getEmail()))
+			throw new NotYourFeedException();
+
+		List<String> photoLinks = new ArrayList<>();
+
+		for(FeedMedium medium : feed.getMedia()) {
+			photoLinks.add(medium.getFileName());
+		}
+
+		photoLinks.forEach(this::removePhoto);
+
+		feedRepository.deleteById(id);
+
+	}
+
+	public void modifyTag(ModifyTagRequest request) {
+		User user = userRepository.findById(userFacade.getEmail())
+				.orElseThrow(CredentialsNotFoundException::new);
+
+		Feed feed = feedRepository.findById(request.getFeedId())
+				.orElseThrow(FeedNotFoundException::new);
+
+		if(!feed.getUser().getEmail().equals(user.getEmail()))
+			throw new NotYourFeedException();
+
+		if(feed.getTags().size() + request.getTags().length > 5)
+			throw new TooManyTagsException();
+
+		for(String tag : request.getTags()) {
+			tagRepository.save(
+					Tag.builder()
+							.feed(feed)
+							.tag(tag)
+					.build()
+			);
+		}
+	}
+
+	private void removePhoto(String fileName) {
+		User user = userRepository.findById(userFacade.getEmail())
+				.orElseThrow(CredentialsNotFoundException::new);
+
+		FeedMedium medium = feedMediumRepository.findByFileName(fileName)
+				.orElseThrow(MediumNotFoundException::new);
+
+		if(!medium.getFeed().getUser().getEmail().equals(user.getEmail()))
+			throw new NotYourFeedException();
+
+		s3Util.delete(medium.getFileName());
+		feedMediumRepository.delete(medium);
+	}
+
 }
--- src/main/java/io/github/tn1/server/service/feed/carrot/CarrotFeedService.java
+++ src/main/java/io/github/tn1/server/service/feed/carrot/CarrotFeedService.java
@@ -1,14 +1,128 @@
 package io.github.tn1.server.service.feed.carrot;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import io.github.tn1.server.dto.feed.request.ModifyCarrotRequest;
 import io.github.tn1.server.dto.feed.request.PostCarrotRequest;
 import io.github.tn1.server.dto.feed.response.FeedElementResponse;
+import io.github.tn1.server.entity.feed.Feed;
+import io.github.tn1.server.entity.feed.FeedRepository;
+import io.github.tn1.server.entity.feed.medium.FeedMedium;
+import io.github.tn1.server.entity.feed.medium.FeedMediumRepository;
+import io.github.tn1.server.entity.feed.tag.Tag;
+import io.github.tn1.server.entity.feed.tag.TagRepository;
+import io.github.tn1.server.entity.like.LikeRepository;
+import io.github.tn1.server.entity.user.User;
+import io.github.tn1.server.entity.user.UserRepository;
+import io.github.tn1.server.exception.CredentialsNotFoundException;
+import io.github.tn1.server.exception.FeedNotFoundException;
+import io.github.tn1.server.exception.NotYourFeedException;
+import io.github.tn1.server.exception.TooManyTagsException;
+import io.github.tn1.server.exception.UserNotFoundException;
+import io.github.tn1.server.facade.feed.FeedFacade;
+import io.github.tn1.server.facade.user.UserFacade;
+import io.github.tn1.server.utils.s3.S3Util;
+import lombok.RequiredArgsConstructor;
 
-public interface CarrotFeedService {
-	void postCarrotFeed(PostCarrotRequest request);
-	void modifyCarrotFeed(ModifyCarrotRequest request);
-	List<FeedElementResponse> queryCarrotFeed(int page, int range);
-	List<FeedElementResponse> queryLikedCarrot();
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class CarrotFeedService {
+
+	private final UserFacade userFacade;
+	private final FeedFacade feedFacade;
+	private final S3Util s3Util;
+	private final FeedRepository feedRepository;
+	private final LikeRepository likeRepository;
+	private final FeedMediumRepository feedMediumRepository;
+	private final TagRepository tagRepository;
+	private final UserRepository userRepository;
+
+	public void postCarrotFeed(PostCarrotRequest request) {
+
+		if(request.getTags() != null && request.getTags().size() > 5)
+			throw new TooManyTagsException();
+
+		User user = userRepository.findById(userFacade.getEmail())
+				.orElseThrow(CredentialsNotFoundException::new);
+
+		Feed feed = feedRepository.save(
+				Feed.builder()
+						.title(request.getTitle())
+						.description(request.getDescription())
+						.price(request.getPrice())
+						.user(user)
+						.isUsedItem(true)
+						.build()
+		);
+
+		if(request.getTags() != null)
+			request.getTags().forEach(tag -> feedFacade.addTag(tag, feed));
+
+	}
+
+	public void modifyCarrotFeed(ModifyCarrotRequest request) {
+		User user = userRepository.findById(userFacade.getEmail())
+				.orElseThrow(CredentialsNotFoundException::new);
+
+		Feed feed = feedRepository.findById(request.getFeedId())
+				.orElseThrow(FeedNotFoundException::new);
+
+		if(!feed.getUser().getEmail().equals(user.getEmail()))
+			throw new NotYourFeedException();
+
+		feed
+				.setTitle(request.getTitle())
+				.setDescription(request.getDescription())
+				.setPrice(request.getPrice());
+
+		feedRepository.save(feed);
+	}
+
+	public List<FeedElementResponse> queryCarrotFeed(int page, int range) {
+
+		User user = userRepository.findById(userFacade.getEmail())
+				.orElse(null);
+
+		return feedRepository.findByIsUsedItem(true, PageRequest.of(page, range, Sort.by("id").descending()))
+				.stream()
+				.map(feed -> {
+					FeedMedium medium = feedMediumRepository
+							.findTopByFeedOrderById(feed);
+					FeedElementResponse response;
+					response = FeedElementResponse.builder()
+							.feedId(feed.getId())
+							.title(feed.getTitle())
+							.price(feed.getPrice())
+							.lastModifyDate(feed.getUpdatedDate())
+							.count(feed.getLikes().size())
+							.description(feed.getDescription())
+							.medium(medium != null ? s3Util.getObjectUrl(medium.getFileName()) : null)
+							.tags(tagRepository.findByFeedOrderById(feed)
+									.stream().map(Tag::getTag).collect(Collectors.toList()))
+							.build();
+					if(user != null) {
+						response.setLike(likeRepository.findByUserAndFeed(user, feed)
+								.isPresent());
+					}
+					return response;
+				}).collect(Collectors.toList());
+	}
+
+	public List<FeedElementResponse> queryLikedCarrot() {
+		User user = userRepository.findById(userFacade.getEmail())
+				.orElseThrow(UserNotFoundException::new);
+		return user.getLikes()
+				.stream().filter(like -> like.getFeed().isUsedItem())
+				.map(like -> {
+					Feed feed = like.getFeed();
+					return feedFacade.feedToFeedResponse(feed, user);
+				}).collect(Collectors.toList());
+	}
+
 }
