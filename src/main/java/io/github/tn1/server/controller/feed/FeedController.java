cfe636d84a2333326951ece357cda98b9600faf7
leeseojune <sung07288346@gmail.com>
2021-10-02 15:36:32 +0900

    ⚡ :: /feed/like

--- src/main/java/io/github/tn1/server/controller/feed/FeedController.java
+++ src/main/java/io/github/tn1/server/controller/feed/FeedController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,6 +56,12 @@ public class FeedController {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void changeTag(@RequestBody @Valid ModifyTagRequest request) {
 		feedService.modifyTag(request);
+	}
+
+	@PostMapping("/like")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void addLike(@RequestParam("feed_id") Long feedId) {
+		feedService.addLike(feedId);
 	}
 
 }
