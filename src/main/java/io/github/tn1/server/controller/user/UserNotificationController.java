8bdeaac023457efb7304af62f827f0a8eda9c7a1
leeseojune <sung07288346@gmail.com>
2021-10-02 23:52:04 +0900

    ⚡ :: DELETE /users/notificatoin

--- src/main/java/io/github/tn1/server/controller/user/UserNotificationController.java
+++ src/main/java/io/github/tn1/server/controller/user/UserNotificationController.java
@@ -4,8 +4,11 @@ import io.github.tn1.server.dto.user.response.NotificationResponse;
 import io.github.tn1.server.service.user.UserNotificationService;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,6 +22,13 @@ public class UserNotificationController {
 	public NotificationResponse queryNotification() {
 		return userNotificationService
 				.queryNotification();
+	}
+
+	@DeleteMapping
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void disableNotification() {
+		userNotificationService
+				.disableNotification();
 	}
 
 }
