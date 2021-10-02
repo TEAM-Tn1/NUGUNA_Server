3d722a428d8d8bb4bc096add4f70a765bc7d7f74
leeseojune <sung07288346@gmail.com>
2021-10-02 17:10:49 +0900

    ⚡ :: deleteByFeed

--- src/main/java/io/github/tn1/server/entity/feed/tag/TagRepository.java
+++ src/main/java/io/github/tn1/server/entity/feed/tag/TagRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
     List<Tag> findByFeedOrderById(Feed feed);
+    void deleteByFeed(Feed feed);
 }
