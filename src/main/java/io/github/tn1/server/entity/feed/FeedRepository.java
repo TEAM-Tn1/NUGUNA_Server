52c23a1156ecfdd4300c2848a2d17c0fcdd17d02
leeseojune <sung07288346@gmail.com>
2021-10-03 17:19:37 +0900

    ⚡ :: findByUserAndIsUsedItem

--- src/main/java/io/github/tn1/server/entity/feed/FeedRepository.java
+++ src/main/java/io/github/tn1/server/entity/feed/FeedRepository.java
@@ -1,15 +1,17 @@
 package io.github.tn1.server.entity.feed;
 
+import java.util.List;
+
 import io.github.tn1.server.entity.user.User;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface FeedRepository extends JpaRepository<Feed, Long> {
     List<Feed> findByUser(User user);
+    List<Feed> findByUserAndIsUsedItem(User user, boolean isUsedItem);
     Page<Feed> findByIsUsedItem(boolean isUsedItem, Pageable pageable);
 }
