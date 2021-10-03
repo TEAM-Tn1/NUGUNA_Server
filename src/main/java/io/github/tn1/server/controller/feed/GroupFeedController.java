0413264a25f779af42542c61a5aab2d28641770e
leeseojune <sung07288346@gmail.com>
2021-10-03 17:09:36 +0900

    ⚡ :: /feed/group

--- src/main/java/io/github/tn1/server/controller/feed/GroupFeedController.java
+++ src/main/java/io/github/tn1/server/controller/feed/GroupFeedController.java
@@ -1,17 +1,22 @@
 package io.github.tn1.server.controller.feed;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import io.github.tn1.server.dto.feed.request.ModifyGroupRequest;
 import io.github.tn1.server.dto.feed.request.PostGroupRequest;
+import io.github.tn1.server.dto.feed.response.GroupResponse;
 import io.github.tn1.server.service.feed.group.GroupFeedService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,6 +37,11 @@ public class GroupFeedController {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void modifyGroupFeed(@RequestBody @Valid ModifyGroupRequest request) {
 		groupFeedService.modifyGroupFeed(request);
+	}
+
+	@GetMapping("/group")
+	public List<GroupResponse> queryGroupFeed(@RequestParam("page") int page, @RequestParam("range") int range) {
+		return groupFeedService.queryGroupFeed(page, range);
 	}
 
 }
