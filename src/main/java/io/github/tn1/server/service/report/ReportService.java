5f8cb43e28e2044a90df3987112d15aeb1fdb258
leeseojune <sung07288346@gmail.com>
2021-10-03 23:27:17 +0900

    ⚡ :: 게시물 신고 로직

--- src/main/java/io/github/tn1/server/service/report/ReportService.java
+++ src/main/java/io/github/tn1/server/service/report/ReportService.java
@@ -1,16 +1,22 @@
 package io.github.tn1.server.service.report;
 
+import io.github.tn1.server.dto.report.request.FeedReportRequest;
 import io.github.tn1.server.dto.report.request.UserReportRequest;
 import io.github.tn1.server.dto.report.response.ReportResponse;
+import io.github.tn1.server.entity.feed.Feed;
+import io.github.tn1.server.entity.feed.FeedRepository;
 import io.github.tn1.server.entity.report.Report;
 import io.github.tn1.server.entity.report.ReportRepository;
 import io.github.tn1.server.entity.report.ReportType;
+import io.github.tn1.server.entity.report.feed_report.FeedReport;
+import io.github.tn1.server.entity.report.feed_report.FeedReportRepository;
 import io.github.tn1.server.entity.report.medium.ReportMedium;
 import io.github.tn1.server.entity.report.medium.ReportMediumRepository;
 import io.github.tn1.server.entity.user.User;
 import io.github.tn1.server.entity.user.UserRepository;
 import io.github.tn1.server.exception.AlreadyReportedUserException;
 import io.github.tn1.server.exception.CredentialsNotFoundException;
+import io.github.tn1.server.exception.FeedNotFoundException;
 import io.github.tn1.server.exception.NotYourReportException;
 import io.github.tn1.server.exception.ReportNotFoundException;
 import io.github.tn1.server.exception.SelfReportException;
@@ -31,6 +37,8 @@ public class ReportService {
 	private final S3Util s3Util;
 	private final UserRepository userRepository;
 	private final ReportRepository reportRepository;
+	private final FeedRepository feedRepository;
+	private final FeedReportRepository feedReportRepository;
 	private final ReportMediumRepository reportMediumRepository;
 
 	public ReportResponse userReport(UserReportRequest request) {
@@ -59,6 +67,48 @@ public class ReportService {
 		} catch (RuntimeException e) {
 			throw new AlreadyReportedUserException();
 		}
+	}
+
+	public ReportResponse feedReport(FeedReportRequest request) {
+		Feed feed = feedRepository
+				.findById(request.getFeedId())
+				.orElseThrow(FeedNotFoundException::new);
+
+		if(feed.getUser().getEmail().equals(userFacade.getEmail()))
+			throw new SelfReportException();
+
+		User currentUser = userRepository
+				.findById(userFacade.getEmail())
+				.orElseThrow(CredentialsNotFoundException::new);
+		User user = feed.getUser();
+
+
+		Report report;
+
+		try {
+			report = reportRepository.save(
+					Report.builder()
+							.title(request.getTitle())
+							.contents(request.getContent())
+							.defendant(user)
+							.reporter(currentUser)
+							.reportType(ReportType.F)
+							.build()
+			);
+		} catch (RuntimeException e) {
+			throw new AlreadyReportedUserException();
+		}
+
+
+
+		feedReportRepository.save(
+				FeedReport.builder()
+				.report(report)
+				.feed(feed)
+				.build()
+		);
+
+		return new ReportResponse(report.getId());
 	}
 
 	public void postMedium(MultipartFile file, Long reportId) {
