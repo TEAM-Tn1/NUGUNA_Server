cc2029db646c814cc82ea76b136c7e92337ac93e
leeseojune <sung07288346@gmail.com>
2021-10-03 14:08:25 +0900

    📑 :: POST /notification/tags

---
+++ src/main/java/io/github/tn1/server/controller/NotificationController.java
@@ -0,0 +1,28 @@
+package io.github.tn1.server.controller;
+
+import io.github.tn1.server.dto.notification.request.TagRequest;
+import io.github.tn1.server.service.notification.NotificationService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notification")
+@RequiredArgsConstructor
+public class NotificationController {
+
+	private final NotificationService notificationService;
+
+	@PostMapping("/tags")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void setNotificationTag(@RequestBody TagRequest request) {
+		notificationService
+				.setNotificationTag(request.getTag());
+	}
+
+}
