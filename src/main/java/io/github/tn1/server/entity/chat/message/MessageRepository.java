6a51ed41a4932d6391336e3938aeb55b2dc3397c
leeseojune <sung07288346@gmail.com>
2021-10-03 23:47:01 +0900

    ⚡ :: MessageRepository findByRoom

--- src/main/java/io/github/tn1/server/entity/chat/message/MessageRepository.java
+++ src/main/java/io/github/tn1/server/entity/chat/message/MessageRepository.java
@@ -1,4 +1,11 @@
 package io.github.tn1.server.entity.chat.message;
 
-public interface MessageRepository {
+import io.github.tn1.server.entity.chat.room.Room;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+	Page<Message> findByRoom(Room room, Pageable pageable);
 }
