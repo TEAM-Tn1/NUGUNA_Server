7a07d8fc95c8507f068ad84cbabf9d53705a3976
leeseojune <sung07288346@gmail.com>
2021-10-03 15:28:55 +0900

    ⚡ :: findByUser

--- src/main/java/io/github/tn1/server/entity/notification/NotificationRepository.java
+++ src/main/java/io/github/tn1/server/entity/notification/NotificationRepository.java
@@ -2,10 +2,13 @@ package io.github.tn1.server.entity.notification;
 
 import io.github.tn1.server.entity.user.User;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 	int countByUser(User user);
+	Page<Notification> findByUser(User user, Pageable pageable);
 }
