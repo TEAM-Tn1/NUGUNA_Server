6a066b923cfb5c0b42d6d75e9aa9ff7df71f35ee
leeseojune <sung07288346@gmail.com>
2021-10-04 21:21:42 +0900

    ⚡ :: countByUserAndIsWatch

--- src/main/java/io/github/tn1/server/entity/notification/NotificationRepository.java
+++ src/main/java/io/github/tn1/server/entity/notification/NotificationRepository.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-	int countByUser(User user);
+	int countByUserAndIsWatch(User user, boolean watch);
 	Page<Notification> findByUser(User user, Pageable pageable);
 }
