7a0f8a63163938c914a7f09aa36026239390dad9
leeseojune <sung07288346@gmail.com>
2021-10-04 22:00:29 +0900

    ⚡ :: GET /question

--- src/main/java/io/github/tn1/server/controller/QuestionController.java
+++ src/main/java/io/github/tn1/server/controller/QuestionController.java
@@ -2,11 +2,14 @@ package io.github.tn1.server.controller;
 
 import javax.validation.Valid;
 
+import io.github.tn1.server.dto.question.request.QuestionDetailRequest;
 import io.github.tn1.server.dto.question.request.QuestionRequest;
+import io.github.tn1.server.dto.question.response.QuestionResponse;
 import io.github.tn1.server.service.question.QuestionService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +27,11 @@ public class QuestionController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public void postQuestion(@RequestBody @Valid QuestionRequest request) {
 		questionService.postQuestion(request);
+	}
+
+	@GetMapping
+	public QuestionResponse queryQuestionDetail(@RequestBody QuestionDetailRequest request) {
+		return questionService.queryQuestionDetail(request);
 	}
 
 }
