8c38516d00f04e6bcdfa4607a274cdb798ab1278
leeseojune <sung07288346@gmail.com>
2021-10-05 23:08:15 +0900

    📑 :: UserReportResultRequest Dto

---
+++ src/main/java/io/github/tn1/server/dto/admin/request/UserReportResultRequest.java
@@ -0,0 +1,26 @@
+package io.github.tn1.server.dto.admin.request;
+
+import java.util.Date;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Getter
+@NoArgsConstructor
+public class UserReportResultRequest {
+
+	@NotNull(message = "report_id는 null이면 안됩니다.")
+	private Long reportId;
+
+	private Date blackDate;
+
+	@NotNull(message = "reason은 null이면 안됩니다.")
+	@Size(max = 250, message = "reason은 250자를 넘어서는 안됩니다.")
+	private String reason;
+
+}
