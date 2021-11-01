de7a79560166653110015465919a354d412c84c3
leeseojune <sung07288346@gmail.com>
2021-11-01 12:04:13 +0900

    ⚡ :: Pageable 예외 핸들링

--- src/main/java/io/github/tn1/server/error/GlobalExceptionHandler.java
+++ src/main/java/io/github/tn1/server/error/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package io.github.tn1.server.error;
 import io.github.tn1.server.error.exception.ErrorCode;
 import io.github.tn1.server.error.exception.ServerException;
 
+import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -25,6 +26,13 @@ public class GlobalExceptionHandler {
     	return new ResponseEntity<>(new ErrorResponse(400,
 				e.getBindingResult().getAllErrors().get(0).getDefaultMessage()),
 				HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(PropertyReferenceException.class)
+	public ResponseEntity<ErrorResponse>
+	propertyReferenceExceptionHandler(PropertyReferenceException e) {
+    	return new ResponseEntity<>(new ErrorResponse(400,
+				e.getMessage()), HttpStatus.BAD_REQUEST);
 	}
 
 }
