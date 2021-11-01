4111bafa50393f4a85e48bd6a034dc1e321efe6c
leeseojune <sung07288346@gmail.com>
2021-11-01 12:05:55 +0900

    ⚡ :: findByReportType pageable

--- src/main/java/io/github/tn1/server/entity/report/ReportRepository.java
+++ src/main/java/io/github/tn1/server/entity/report/ReportRepository.java
@@ -2,10 +2,11 @@ package io.github.tn1.server.entity.report;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
-	List<Report> findByReportType(ReportType reportType);
+	List<Report> findByReportType(ReportType reportType, Pageable pageable);
 }
