0cd68f3a36fb8afec13b3cb81295452e82e425b6
leeseojune <sung07288346@gmail.com>
2021-11-10 03:18:12 +0900

    ⚡ :: getCurrentUser

--- src/main/java/io/github/tn1/server/facade/user/UserFacade.java
+++ src/main/java/io/github/tn1/server/facade/user/UserFacade.java
@@ -1,20 +1,32 @@
 package io.github.tn1.server.facade.user;
 
+import io.github.tn1.server.entity.user.User;
+import io.github.tn1.server.entity.user.UserRepository;
 import io.github.tn1.server.exception.CredentialsNotFoundException;
+import lombok.RequiredArgsConstructor;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class UserFacade {
 
-    public String getEmail() {
+	private final UserRepository userRepository;
+
+    public String getCurrentEmail() {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if(authentication == null || authentication.getPrincipal() == null)
             throw new CredentialsNotFoundException();
 
         return authentication.getName();
     }
+
+    public User getCurrentUser() {
+    	return userRepository
+				.findById(getCurrentEmail())
+				.orElseThrow(CredentialsNotFoundException::new);
+	}
 
 }
