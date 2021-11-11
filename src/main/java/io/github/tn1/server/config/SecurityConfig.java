0541f4608344aaedd2addf5278ebd0914a77a031
leeseojune <sung07288346@gmail.com>
2021-11-11 21:36:03 +0900

    ⚡ :: permitall /feed/search

--- src/main/java/io/github/tn1/server/config/SecurityConfig.java
+++ src/main/java/io/github/tn1/server/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.PUT, "/users/auth").permitAll()
                 .antMatchers(HttpMethod.GET, "/users/{email}").permitAll()
 
+				.antMatchers(HttpMethod.GET, "/feed/search").permitAll()
 				.antMatchers(HttpMethod.GET, "/feed/tag").permitAll()
                 .antMatchers(HttpMethod.GET, "/feed/users/{email}").permitAll()
 				.antMatchers(HttpMethod.GET, "/feed/{feed_id}").permitAll()
