c04c339e2b23da409dc4cd6b90e48a153cb176c3
leeseojune <sung07288346@gmail.com>
2021-11-11 21:34:47 +0900

    ⚡ :: findByTitleContainsAndIsUsedItem

--- src/main/java/io/github/tn1/server/entity/feed/FeedRepository.java
+++ src/main/java/io/github/tn1/server/entity/feed/FeedRepository.java
@@ -14,4 +14,5 @@ public interface FeedRepository extends JpaRepository<Feed, Long> {
     List<Feed> findByUser(User user);
     List<Feed> findByUserAndIsUsedItem(User user, boolean isUsedItem);
     Page<Feed> findByIsUsedItem(boolean isUsedItem, Pageable pageable);
+    Page<Feed> findByTitleContainsAndIsUsedItem(String title, boolean isUsedItem, Pageable pageable);
 }
