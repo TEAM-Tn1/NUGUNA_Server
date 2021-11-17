9ccf87d42c0c4052d12769f95c60f8c93c673d40
leeseojune <sung07288346@gmail.com>
2021-11-17 11:12:46 +0900

    ⚡ :: findByUser

--- src/main/java/io/github/tn1/server/domain/like/domain/repository/LikeRepository.java
+++ src/main/java/io/github/tn1/server/domain/like/domain/repository/LikeRepository.java
@@ -1,14 +1,18 @@
 package io.github.tn1.server.domain.like.domain.repository;
 
+import java.util.Optional;
+
 import io.github.tn1.server.domain.feed.domain.Feed;
 import io.github.tn1.server.domain.like.domain.Like;
 import io.github.tn1.server.domain.user.domain.User;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
     Optional<Like> findByUserAndFeed(User user, Feed feed);
+    Page<Like> findByUser(User user, Pageable pageable);
 }
