7f6c0c1269b70525f99f78375ecdf9e4d60de5c1
leeseojune <sung07288346@gmail.com>
2021-11-18 14:35:46 +0900

    ⚡ :: Socket Config

--- src/main/java/io/github/tn1/server/global/config/WebSocketConfig.java
+++ src/main/java/io/github/tn1/server/global/config/WebSocketConfig.java
@@ -1,26 +1,37 @@
 package io.github.tn1.server.global.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOServer;
+import io.github.tn1.server.global.socket.WebSocketAddMappingSupporter;
+import io.github.tn1.server.global.socket.exception.SocketExceptionListener;
+import io.github.tn1.server.global.socket.security.WebSocketConnectController;
+import lombok.RequiredArgsConstructor;
 
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
-	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry
-				.addEndpoint("/chat")
-				.setAllowedOriginPatterns("*")
-				.withSockJS();
+@Component
+@RequiredArgsConstructor
+public class WebSocketConfig {
+
+	private final WebSocketAddMappingSupporter mappingSupporter;
+	private final WebSocketConnectController connectController;
+	private final SocketExceptionListener exceptionListener;
+
+	@Value("${socket.port}")
+	private Integer port;
+
+	@Bean
+	public SocketIOServer socketIOServer() {
+		Configuration config = new Configuration();
+		config.setPort(port);
+		config.setOrigin("*");
+		config.setExceptionListener(exceptionListener);
+		SocketIOServer server = new SocketIOServer(config);
+		mappingSupporter.addListeners(server);
+		server.addConnectListener(connectController::onConnect);
+		return server;
 	}
 
-	@Override
-	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		registry.setApplicationDestinationPrefixes("/pub");
-		registry.enableSimpleBroker("/sub");
-	}
 }
