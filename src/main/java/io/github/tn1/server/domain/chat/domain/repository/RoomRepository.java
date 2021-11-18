49a858dd336c6202d8df64ba684391b142b833f9
leeseojune <sung07288346@gmail.com>
2021-11-19 01:13:29 +0900

    🐛 :: List

--- src/main/java/io/github/tn1/server/domain/chat/domain/repository/RoomRepository.java
+++ src/main/java/io/github/tn1/server/domain/chat/domain/repository/RoomRepository.java
@@ -1,5 +1,6 @@
 package io.github.tn1.server.domain.chat.domain.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import io.github.tn1.server.domain.chat.domain.Room;
@@ -15,7 +16,7 @@ public interface RoomRepository extends CrudRepository<Room, String> {
 	Optional<Room> findByFeed(Feed feed);
 
 	@Query("select r from tbl_room r join tbl_room_member m on m.user.email = :email where r.id = m.room.id and r.type = :type")
-	Optional<Room> findByEmailAndType(String email, RoomType type);
+	List<Room> findByEmailAndType(String email, RoomType type);
 
 	@Query("select case when count(r.id) > 0 then true else false end from tbl_room r join tbl_room_member m on m.user.email = :email where r.feed = :feed and r.id = m.room.id")
 	boolean existsByFeedAndEmail(Feed feed, String email);
