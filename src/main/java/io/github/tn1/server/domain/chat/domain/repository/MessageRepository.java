9ecbd331d314a23e4aa8774054fd5e91056614be
leeseojune <sung07288346@gmail.com>
2021-11-20 17:42:24 +0900

    ⚡ :: setNullByMember

--- src/main/java/io/github/tn1/server/domain/chat/domain/repository/MessageRepository.java
+++ src/main/java/io/github/tn1/server/domain/chat/domain/repository/MessageRepository.java
@@ -8,8 +8,14 @@ import io.github.tn1.server.domain.chat.domain.Room;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
 	Page<Message> findByRoom(Room room, Pageable pageable);
 	Optional<Message> findTopByRoom(Room room);
+
+	@Modifying
+	@Query("update tbl_message m set m.member = null where m.member.id = :memberId")
+	void setNullByMember(Long memberId);
 }
