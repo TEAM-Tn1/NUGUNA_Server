2e24fd096082364f11cda5fdd532418afdb21a12
leeseojune <sung07288346@gmail.com>
2021-11-20 14:34:02 +0900

    ⚰ :: join room api

--- src/main/java/io/github/tn1/server/domain/chat/service/ChatService.java
+++ src/main/java/io/github/tn1/server/domain/chat/service/ChatService.java
@@ -13,25 +13,17 @@ import io.github.tn1.server.domain.chat.domain.repository.MessageRepository;
 import io.github.tn1.server.domain.chat.domain.repository.RoomRepository;
 import io.github.tn1.server.domain.chat.domain.types.MessageType;
 import io.github.tn1.server.domain.chat.domain.types.RoomType;
-import io.github.tn1.server.domain.chat.exception.AlreadyJoinRoomException;
 import io.github.tn1.server.domain.chat.exception.NotYourRoomException;
-import io.github.tn1.server.domain.chat.exception.RoomIsFullException;
-import io.github.tn1.server.domain.chat.exception.RoomNotFoundException;
 import io.github.tn1.server.domain.chat.facade.RoomFacade;
 import io.github.tn1.server.domain.chat.presentation.dto.request.ChatRequest;
 import io.github.tn1.server.domain.chat.presentation.dto.request.QueryMessageRequest;
 import io.github.tn1.server.domain.chat.presentation.dto.response.CarrotRoomResponse;
 import io.github.tn1.server.domain.chat.presentation.dto.response.GroupRoomResponse;
-import io.github.tn1.server.domain.chat.presentation.dto.response.JoinResponse;
 import io.github.tn1.server.domain.chat.presentation.dto.response.QueryMessageResponse;
-import io.github.tn1.server.domain.feed.domain.Feed;
-import io.github.tn1.server.domain.feed.domain.FeedMedium;
 import io.github.tn1.server.domain.feed.domain.repository.FeedMediumRepository;
 import io.github.tn1.server.domain.feed.facade.FeedFacade;
-import io.github.tn1.server.domain.report.exception.ItsYourFeedException;
 import io.github.tn1.server.domain.user.domain.User;
 import io.github.tn1.server.domain.user.facade.UserFacade;
-import io.github.tn1.server.global.utils.s3.S3Util;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.data.domain.PageRequest;
@@ -42,64 +34,10 @@ import org.springframework.stereotype.Service;
 public class ChatService {
 
 	private final UserFacade userFacade;
-	private final FeedFacade feedFacade;
-	private final FeedMediumRepository feedMediumRepository;
 	private final RoomRepository roomRepository;
 	private final RoomFacade roomFacade;
 	private final MessageRepository messageRepository;
 	private final MemberRepository memberRepository;
-
-	private final S3Util s3Util;
-
-	@Transactional
-	public JoinResponse joinRoom(Long feedId) {
-		User currentUser = userFacade.getCurrentUser();
-
-		Feed feed = feedFacade.getFeedById(feedId);
-
-		FeedMedium medium = feedMediumRepository
-				.findTopByFeedOrderById(feed);
-		Room room;
-
-		if(feed.isWriter(currentUser.getEmail()))
-			throw new ItsYourFeedException();
-
-		if(roomRepository.existsByFeedAndEmail(feed, currentUser.getEmail())){
-			throw new AlreadyJoinRoomException();
-		}
-
-		if(feed.isUsedItem()) {
-			room = roomRepository.save(
-					Room.builder()
-							.feed(feed)
-							.user(feed.getUser())
-							.type(RoomType.CARROT)
-							.photoUrl(medium != null ?
-									s3Util.getObjectUrl(medium.getFileName()) : null)
-							.build());
-			memberRepository.save(
-					Member.builder()
-							.user(feed.getUser())
-							.room(room)
-							.build()
-			);
-		} else {
-			if(feed.getGroup().getCurrentCount() >=
-					feed.getGroup().getHeadCount())
-				throw new RoomIsFullException();
-			feed.getGroup().increaseCurrentCount();
-			room = roomRepository.findByFeed(feed)
-					.orElseThrow(RoomNotFoundException::new);
-		}
-		memberRepository.save(
-				Member.builder()
-						.user(currentUser)
-						.room(room)
-						.build()
-		);
-
-		return new JoinResponse(room.getId());
-	}
 
 	public List<CarrotRoomResponse> queryCarrotRoom() {
 		return roomRepository.findByEmailAndType(userFacade.getCurrentEmail(),
