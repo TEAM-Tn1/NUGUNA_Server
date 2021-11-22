e2d2bcd0c677945542e766731b6bc84daad7f3c8
leeseojune <sung07288346@gmail.com>
2021-11-23 00:42:19 +0900

    ♻ :: snake case로 통일

--- src/main/java/io/github/tn1/server/domain/chat/presentation/dto/MessageDto.java
+++ src/main/java/io/github/tn1/server/domain/chat/presentation/dto/MessageDto.java
@@ -1,5 +1,6 @@
 package io.github.tn1.server.domain.chat.presentation.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.github.tn1.server.domain.chat.domain.types.MessageType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,12 +11,15 @@ import lombok.Getter;
 @AllArgsConstructor
 public class MessageDto {
 
+	@JsonProperty("room_id")
 	private final String roomId;
 
+	@JsonProperty("message_id")
 	private final Long messageId;
 
 	private final String content;
 
+	@JsonProperty("sent_at")
 	private final String sentAt;
 
 	private final MessageType type;
--- src/main/java/io/github/tn1/server/domain/chat/presentation/dto/request/ChatRequest.java
+++ src/main/java/io/github/tn1/server/domain/chat/presentation/dto/request/ChatRequest.java
@@ -1,5 +1,6 @@
 package io.github.tn1.server.domain.chat.presentation.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ public class ChatRequest {
 
 	private String message;
 
+	@JsonProperty("room_id")
 	private String roomId;
 
 }
