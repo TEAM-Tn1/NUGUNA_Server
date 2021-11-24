ec11fd60fd7065e24db80254ed34cd5a7514bb0b
leeseojune <sung07288346@gmail.com>
2021-11-25 00:00:38 +0900

    ⚡ :: query string로 요청

--- src/main/java/io/github/tn1/server/domain/chat/presentation/ChatController.java
+++ src/main/java/io/github/tn1/server/domain/chat/presentation/ChatController.java
@@ -2,9 +2,6 @@ package io.github.tn1.server.domain.chat.presentation;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
-import io.github.tn1.server.domain.chat.presentation.dto.request.QueryMessageRequest;
 import io.github.tn1.server.domain.chat.presentation.dto.response.CarrotRoomResponse;
 import io.github.tn1.server.domain.chat.presentation.dto.response.GroupRoomResponse;
 import io.github.tn1.server.domain.chat.presentation.dto.response.QueryMessageResponse;
@@ -12,7 +9,6 @@ import io.github.tn1.server.domain.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,8 +31,8 @@ public class ChatController {
 	}
 
 	@GetMapping("/content")
-	public List<QueryMessageResponse> queryMessage(@RequestBody @Valid QueryMessageRequest request, @RequestParam("page") int page) {
-		return chatService.queryMessage(request, page);
+	public List<QueryMessageResponse> queryMessage(@RequestParam("page") int page, @RequestParam("room_id") String roomId) {
+		return chatService.queryMessage(roomId, page);
 	}
 
 }
