284ec3eebe257dc1d6cf5a32d0127928cf9b51b5
leeseojune <sung07288346@gmail.com>
2021-11-29 11:14:05 +0900

    ⚡ :: deleteByFeedId

--- src/main/java/io/github/tn1/server/domain/feed/domain/repository/TagRepository.java
+++ src/main/java/io/github/tn1/server/domain/feed/domain/repository/TagRepository.java
@@ -1,15 +1,20 @@
 package io.github.tn1.server.domain.feed.domain.repository;
 
+import java.util.List;
+
 import io.github.tn1.server.domain.feed.domain.Feed;
 import io.github.tn1.server.domain.feed.domain.Tag;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
     List<Tag> findByFeedOrderById(Feed feed);
-    void deleteByFeed(Feed feed);
+
+    @Modifying
+    @Query("delete from tbl_tag t where t.feed.id = :feedId")
+    void deleteByFeedId(Long feedId);
 }
