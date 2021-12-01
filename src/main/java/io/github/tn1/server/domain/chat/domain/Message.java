02fb5b354d4f7e5e2b68c9793a6645f63051e618
leeseojune <sung07288346@gmail.com>
2021-12-02 01:21:50 +0900

    🐛 :: casecade

--- src/main/java/io/github/tn1/server/domain/chat/domain/Message.java
+++ src/main/java/io/github/tn1/server/domain/chat/domain/Message.java
@@ -2,7 +2,6 @@ package io.github.tn1.server.domain.chat.domain;
 
 import java.time.LocalDateTime;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
@@ -51,7 +50,7 @@ public class Message {
 	@JoinColumn(name = "member_id")
 	private Member member;
 
-	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "room_id")
 	private Room room;
 
